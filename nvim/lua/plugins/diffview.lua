b622c1d4f2f4589ab4ae0d1bf2b5d201198a93ab
atomic-235 <github.viral908@passmail.net>
2026-07-14 13:43:42 +0400

    nvim: add <leader>gd for DiffviewOpen (uncommitted changes)

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -2,6 +2,7 @@ return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewFileHistory" },
   keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Uncommitted" },
     { "<leader>gF", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview Branch History" },
   },
   opts = {},
