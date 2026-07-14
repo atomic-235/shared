ce8a5e41d8a89af74d040590394a9e09b209c4cb
atomic-235 <github.viral908@passmail.net>
2026-07-14 15:02:33 +0400

    nvim: diffview <leader>e toggles file panel, remove <leader>b

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -5,5 +5,23 @@ return {
     { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview Uncommitted" },
     { "<leader>gF", "<cmd>DiffviewFileHistory<cr>", desc = "Diffview Branch History" },
   },
-  opts = {},
+  opts = function()
+    local actions = require("diffview.actions")
+    return {
+      keymaps = {
+        view = {
+          { "n", "<leader>e", actions.toggle_files, { desc = "Toggle file panel" } },
+          { "n", "<leader>b", false },
+        },
+        file_panel = {
+          { "n", "<leader>e", actions.toggle_files, { desc = "Toggle file panel" } },
+          { "n", "<leader>b", false },
+        },
+        file_history_panel = {
+          { "n", "<leader>e", actions.toggle_files, { desc = "Toggle file panel" } },
+          { "n", "<leader>b", false },
+        },
+      },
+    }
+  end,
 }
