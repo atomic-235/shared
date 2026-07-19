f8c6cc69d6ac77181f0ef184a5a641d39940fceb
atomic-235 <github.viral908@passmail.net>
2026-07-19 10:41:32 +0400

    feat: usql module, remove litecli

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
         shell-tools = import ./modules/shell-tools.nix;
         neovim = import ./modules/neovim.nix;
         lazygit = import ./modules/lazygit.nix;
-        litecli = import ./modules/litecli.nix;
+        usql = import ./modules/usql.nix;
         opencode = import ./modules/opencode.nix;
         bash = import ./modules/bash.nix;
         yazi = import ./modules/yazi.nix;
