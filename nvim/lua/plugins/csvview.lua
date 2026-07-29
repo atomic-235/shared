08a270bfe8e4bb024b6e611a693432fb729de330
atomic-235 <github.viral908@passmail.net>
2026-07-29 11:44:43 +0400

    feat(nvim): add csvview.nvim for CSV viewing

---
+++ nvim/lua/plugins/csvview.lua
@@ -0,0 +1,22 @@
+-- CSV viewer with async parsing and virtual text rendering.
+-- Handles medium-large CSVs without modifying buffer content.
+-- For truly massive files (>100MB), use external tools (csvlens, qsv).
+return {
+  "hat0uma/csvview.nvim",
+  ---@module "csvview"
+  ---@type CsvView.Options
+  opts = {
+    parser = {
+      async_chunksize = 50,
+      limit_max_col_count = 200,
+    },
+    view = {
+      display_mode = "border",
+    },
+  },
+  cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+  ft = { "csv", "tsv" },
+  keys = {
+    { "<leader>tc", "<cmd>CsvViewToggle<cr>", desc = "Toggle CSV view" },
+  },
+}
