b0c32302dae3b1d1c3965c1b180ca754b2efc59b
atomic-235 <github.viral908@passmail.net>
2026-08-06 20:45:48 +0400

    fix: remove bogus neovim spec and snacks terminal override (broke lazygit)

--- nvim/lua/plugins/disable-terminal.lua
+++ nvim/lua/plugins/disable-terminal.lua
@@ -8,27 +8,10 @@ return {
       { "<c-/>", false },
       { "<c-_>", false },
     },
-  },
-
-  -- No-op the snacks.terminal wrapper
-  {
-    "folke/snacks.nvim",
-    opts = {
-      terminal = {
-        override = function()
-          vim.notify("Terminal disabled in this config", vim.log.levels.WARN)
-        end,
-      },
-    },
-  },
-
-  -- Shadow the built-in :terminal ex-command (builtins can't be deleted)
-  {
-    "neovim",
-    opts = function()
-      vim.api.nvim_create_user_command("Terminal", function()
+    init = function()
+      vim.api.nvim_create_user_command("terminal", function()
         vim.notify("Terminal disabled in this config", vim.log.levels.WARN)
-      end, { nargs = "*", desc = "Terminal (disabled)" })
+      end, { nargs = "*", desc = "terminal (disabled)" })
     end,
   },
 }
