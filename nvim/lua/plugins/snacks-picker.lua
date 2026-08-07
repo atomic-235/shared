e43c53153ecd44b16e52722065e889ef72bb805d
atomic-235 <github.viral908@passmail.net>
2026-08-07 12:49:53 +0400

    feat: remap <leader><leader> to smart picker (frecency sorted)

--- nvim/lua/plugins/snacks-picker.lua
+++ nvim/lua/plugins/snacks-picker.lua
@@ -1,5 +1,8 @@
 return {
   "folke/snacks.nvim",
+  keys = {
+    { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+  },
   opts = {
     picker = {
       sources = {
