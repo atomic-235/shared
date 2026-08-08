af9f4c7d99b56f8b056ec5a8e1c4d948ce46120d
atomic-235 <github.viral908@passmail.net>
2026-08-08 21:56:15 +0400

    yazi: force overwrite in compress keymaps

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -53,27 +53,27 @@ in
       mgr.prepend_keymap = [
         {
           on = [ "c" "z" ];
-          run = ''shell --block -- sh -c 'tar -cf - %s | zstd -3 -o archive.tar.zst && echo "Done: archive.tar.zst" || echo "FAILED"; read -r -p "Press Enter to continue..."' '';
+          run = ''shell --block -- sh -c 'out="archive.tar.zst"; tar -cf - %s | zstd -3 -fo "$out" && echo "Done: $out" || echo "FAILED"; read -r -p "Press Enter..."' '';
           desc = "Compress to tar.zst";
         }
         {
           on = [ "c" "g" ];
-          run = ''shell --block -- sh -c 'tar -czf archive.tar.gz %s && echo "Done: archive.tar.gz" || echo "FAILED"; read -r -p "Press Enter to continue..."' '';
+          run = ''shell --block -- sh -c 'out="archive.tar.gz"; tar -czf "$out" %s && echo "Done: $out" || echo "FAILED"; read -r -p "Press Enter..."' '';
           desc = "Compress to tar.gz";
         }
         {
           on = [ "c" "t" ];
-          run = ''shell --block -- sh -c 'tar -cf archive.tar %s && echo "Done: archive.tar" || echo "FAILED"; read -r -p "Press Enter to continue..."' '';
+          run = ''shell --block -- sh -c 'out="archive.tar"; tar -cf "$out" %s && echo "Done: $out" || echo "FAILED"; read -r -p "Press Enter..."' '';
           desc = "Compress to tar";
         }
         {
           on = [ "c" "l" ];
-          run = ''shell --block -- sh -c 'tar -cf - %s | lz4 -1 - archive.tar.lz4 && echo "Done: archive.tar.lz4" || echo "FAILED"; read -r -p "Press Enter to continue..."' '';
+          run = ''shell --block -- sh -c 'out="archive.tar.lz4"; tar -cf - %s | lz4 -1 -f - "$out" && echo "Done: $out" || echo "FAILED"; read -r -p "Press Enter..."' '';
           desc = "Compress to tar.lz4";
         }
         {
           on = [ "c" "x" ];
-          run = ''shell --block -- sh -c 'tar -cf - %s | xz -0 -T0 - archive.tar.xz && echo "Done: archive.tar.xz" || echo "FAILED"; read -r -p "Press Enter to continue..."' '';
+          run = ''shell --block -- sh -c 'out="archive.tar.xz"; tar -cf - %s | xz -0 -T0 -f - > "$out" && echo "Done: $out" || echo "FAILED"; read -r -p "Press Enter..."' '';
           desc = "Compress to tar.xz";
         }
       ];
